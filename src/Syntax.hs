05d2b6e09fba0cb86ac4c6f9ccc6f913d474a69d
Isaac Elliott <isaace71295@gmail.com>
2018-08-14 18:02:58 +1000

    Progress

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,14 +1,17 @@
 {-# language GADTs, DataKinds, StandaloneDeriving #-}
 module Syntax where
 
-data Constructor
-data Destructor
+data Constructor = Type
+  deriving (Eq, Show)
+
+data Destructor = DD
+  deriving (Eq, Show)
 
 data Cat = C | S
 
 data Syntax cat where
   Constructor :: Constructor -> Syntax 'C
-  Lambda :: Syntax 'S -> Syntax 'C
+  Bracket :: Syntax 'S -> Syntax 'C
   Var :: Int -> Syntax 'S
   Ann :: Syntax 'C -> Syntax 'C -> Syntax 'S
   App :: Syntax 'S -> Destructor -> Syntax 'S
@@ -18,11 +21,24 @@ deriving instance Show (Syntax cat)
 -- | head = most recently bound variable
 type Context = [Syntax 'C]
 
-isTypeOf :: Context -> Syntax 'C -> Syntax 'C -> Bool
-isTypeOf = undefined
+index :: Int -> [a] -> Maybe a
+index 0 (a:_) = Just a
+index n (_:as)
+  | n < 0 = Nothing
+  | otherwise = index (n-1) as
 
-hasType :: Context -> Syntax 'S -> Maybe (Syntax 'C)
-hasType (Var n) = context !! n
+isTypeOf :: Context -> Syntax 'C -> Syntax 'C -> Bool
+isTypeOf context ty (Bracket e) =
+  case inferType context e of
+    Nothing -> False
+    Just ty' -> ty == ty'
+
+inferType :: Context -> Syntax 'S -> Maybe (Syntax 'C)
+inferType context (Var n) = index n context
+inferType context (Ann tm ty)
+  | isTypeOf context (Constructor Type) ty, isTypeOf context ty tm = Just tm
+  | otherwise = Nothing
+inferType context s = undefined
 
 step :: Syntax cat -> Maybe (Syntax cat)
 step = undefined
